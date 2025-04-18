e6370005ebb8e525c528e6627170a8efe90b1b2e
Rashi <rashikudale2002@gmail.com>
2025-04-18 13:10:24 +0530

    Add new files

---
+++ src/main/java/com/mycompany/hello/java/maven/HelloJavaMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hello.java.maven;
+
+/**
+ *
+ * @author admin
+ */
+public class HelloJavaMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
